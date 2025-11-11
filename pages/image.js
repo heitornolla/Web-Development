19aefc40d286e72ec40e589e814392d5ab3c9a6a
heitor <heitor.nolla@gmail.com>
2025-11-10 21:24:29 -0300

    exploring images

---
+++ pages/image.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <img
+      src="https://cdn.meutimao.com.br/_upload/torcida-do-corinthians/g/foto/844/lucas-arthur-melo-guari-844267.jpg"
+      alt="Torcida do Corinthians"
+      width="500"
+      height="350"
+    />
+  );
+}

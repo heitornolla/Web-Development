87314192afa75f6337f18a57008803447a8fb4b9
heitor <heitor.nolla@gmail.com>
2025-11-10 21:34:56 -0300

    restore base home

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["cdn.meutimao.com.br"],
-  },
-};
-
-module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 function Home() {
   return (
     <div>
       <h1>Teste</h1>
-
-      <p>
-        Cool Image: <Link href="/image">here</Link>.
-      </p>
     </div>
   );
 }
